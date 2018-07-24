92456cc4768fd0d73b457ab13f0140c48118522b
KPetersone <kristina.petersone@testdevlab.com>
2018-07-24 17:39:57 +0300

    Created gitignore file

---
+++ src/test/java/stepdefinitions/LoginSteps.java
@@ -0,0 +1,13 @@
+LoginSteps.java
+
+        package stepdefinitions;
+
+import cucumber.api.PendingException;
+import cucumber.api.java.en.And;
+
+public class LoginSteps {
+    @And("^I select Login button$")
+    public void iSelectLoginButton() throws Throwable {
+        selectLoginbutton();
+    }
+}
