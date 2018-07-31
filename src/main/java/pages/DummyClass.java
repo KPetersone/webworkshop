b50da9a0a9f87a98ef5d7cc27bda41ee28b56938
KPetersone <kristina.petersone@testdevlab.com>
2018-07-31 17:19:12 +0300

    3.lekcijas pilnais saturss

--- src/main/java/pages/DummyClass.java
+++
@@ -1,7 +0,0 @@
-package pages.header;
-        import com.codeborne.selenide.SelenideElement;
-
-        import static com.codeborne.selenide.SelenideElement;
-
-public class DummyClass {
-}
--- src/main/java/pages/header/NavigationPageObject.java
+++ src/main/java/pages/header/NavigationPageObject.java
@@ -1,10 +1,41 @@
-package pages.header;
-import com.codeborne.selenide.SelenideElement;
+ackage pages.header;
 
-import static com.codeborne.selenide.SelenideElement;
+import com.codeborne.selenide.SelenideElement;
+import pages.logout.LogOutPageObject;
+import pages.signup.SignUpPageObject;
+
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.page;
 
 public class NavigationPageObject {
-    private SelenideElement myAccountButton = $(cssSelector:"test");
-    private SelenideElement getMyAccountButton = $(cssSelector:"test");
 
+    private SelenideElement myAccountButton = $("test");
+
+    private SelenideElement getMyAccountButton() {
+
+        return $("test");
+    }
+
+    private SelenideElement getSignUpButton() {
+        return $("test");
+    }
+
+    private SelenideElement getLogOutButton() {
+        return $("test");
+    }
+
+    public void selectMyAccounButton() {
+
+        getMyAccountButton().click();
+    }
+
+    public SignUpPageObject selectSignUpButton() {
+        getSignUpButton().click();
+        return page(SignUpPageObject.class);
+    }
+
+    public LogOutPageObject selectLogOutButton() {
+        getLogOutButton().click();
+        return page(LogOutPageObject.class);
+    }
 }
